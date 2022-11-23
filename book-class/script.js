fc16591165e9526b9421ac12e8ed0364e2bd3f82
OzanOcak <ozanocak@gmail.com>
2022-11-22 23:42:59 -0500

    firt commit

---
+++ book-class/script.js
@@ -0,0 +1,43 @@
+class Book {
+  constructor(title, author, ISBN, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+  }
+
+  // Getter
+  get availability() {
+    return this.getAvailability();
+  }
+
+  // Methods
+  getAvailability() {
+    if (this.numCopies === 0) {
+      return "Out of stock";
+    } else if (this.numCopies < 10) {
+      return "Low stock";
+    }
+    return "In stock";
+  }
+
+  getNumberOfBook() {
+    return this.numCopies;
+  }
+
+  sell(numCopiesSold = 1) {
+    this.numCopies -= numCopiesSold;
+  }
+
+  restock(numCopiesStocked = 5) {
+    this.numCopies += numCopiesStocked;
+  }
+}
+
+const HungerGames = new Book("Hunger Games", "Suzanne Collins", 123919, 5);
+console.log(HungerGames.availability);
+HungerGames.restock(12);
+console.log(HungerGames.availability);
+HungerGames.sell(7);
+console.log(HungerGames.availability);
+console.log(HungerGames.getNumberOfBook());
