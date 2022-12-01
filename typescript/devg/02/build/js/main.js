080a94642b7f5292ae8f0f2d39afd2611cf43000
OzanOcak <ozanocak@gmail.com>
2022-12-01 18:08:04 -0500

    ts

--- typescript/devg/02/build/js/main.js
+++ typescript/devg/02/build/js/main.js
@@ -1,14 +1,47 @@
 "use strict";
-let name1 = "jerry";
-let favoriteNumber = 5;
-let isLoading = true;
-let anyNumber;
-name1 = "tom";
-favoriteNumber = 23;
-console.log(name1, favoriteNumber);
-const sum = (a, b) => a + b;
-console.log(sum(5, 2));
-anyNumber = 5;
-anyNumber = "str";
-let strOrNumber;
-let reg = /\+w/g;
+let strArr = ["a", "b", "c"];
+strArr[0] = "d";
+console.log(strArr); // (3) ['d', 'b', 'c']
+// strArr[1] = 5; a string array
+console.log(strArr); //(4) ['d', 5, 'c', 3]
+let mixedArr = [2, "a", true]; //we can only add these types but index can be different
+mixedArr = ["b", false, 4]; //(3) ['b', false, 4]
+let digits = [];
+//--------tuple : same index- same type
+let tuple = ["a", 5, false];
+tuple[2] = true;
+//-------- Objects
+let obj;
+obj = [];
+console.log(typeof obj); // object
+obj = strArr;
+obj = {};
+const obj2 = {
+    prop1: "ho",
+    prop2: "ha"
+};
+obj2.prop1 = "hi";
+let robot1 = {
+    namey: "oppy",
+    service_year: [2004, 2005, 2013],
+    active: false
+};
+let robot2 = {
+    service_year: [2015],
+    active: true
+};
+const greet = (robo1t) => {
+    var _a;
+    return `hello my name is ${(_a = robot1.namey) === null || _a === void 0 ? void 0 : _a.toUpperCase()}`; // or if(robot.namey)
+};
+console.log(greet(robot1));
+//----------enum
+var Grade;
+(function (Grade) {
+    Grade[Grade["A"] = 0] = "A";
+    Grade[Grade["B"] = 1] = "B";
+    Grade[Grade["C"] = 2] = "C";
+    Grade[Grade["D"] = 3] = "D";
+    Grade[Grade["F"] = 45] = "F";
+})(Grade || (Grade = {}));
+console.log(Grade.F); // 45
--- typescript/devg/02/src/main.ts
+++ typescript/devg/02/src/main.ts
@@ -1,23 +1,68 @@
-let name1:string = "jerry"
-let favoriteNumber: number = 5
-let isLoading: boolean =  true
+let strArr = ["a", "b", "c"];
+strArr[0] = "d";
+console.log(strArr); // (3) ['d', 'b', 'c']
+// strArr[1] = 5; a string array
+console.log(strArr); //(4) ['d', 5, 'c', 3]
 
-let anyNumber:any
-
-name1="tom"
-
-favoriteNumber =23
-
-console.log(name1,favoriteNumber)
-
-const sum = (a:number,b:number):number => a+b
-console.log(sum(5,2))
+let mixedArr = [2,"a",true] //we can only add these types but index can be different
+mixedArr=["b",false,4] //(3) ['b', false, 4]
 
 
-anyNumber=5
+let digits: number[]= []
 
-anyNumber="str"
+//--------tuple : same index- same type
 
-let strOrNumber: string | number
+let tuple:[string,number,boolean]=["a",5,false]
 
-let reg:RegExp = /\+w/g
+tuple[2]=true
+
+
+//-------- Objects
+
+let obj: object
+obj=[]
+console.log(typeof obj); // object
+obj = strArr
+obj={}
+
+const obj2={
+    prop1:"ho",
+    prop2:"ha"
+}
+obj2.prop1="hi"
+
+
+// ---- interface
+interface Robot{
+    namey?:string,
+    service_year: number[],
+    active:boolean
+}
+
+let robot1:Robot = {
+    namey: "oppy",
+    service_year: [2004,2005,2013],
+    active: false
+}
+
+let robot2:Robot={
+    service_year:[2015],
+    active:true
+}
+
+const greet = (robo1t:Robot) => {
+    return `hello my name is ${robot1.namey?.toUpperCase()}`  // or if(robot.namey)
+}
+console.log(greet(robot1))
+
+//----------enum
+
+enum Grade{
+    A,
+    B,
+    C,
+    D,
+    F=45,
+}
+
+console.log(Grade.F); // 45
